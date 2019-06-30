e7f895f9765a52b1ef3100dd3fbda079ea325728
dschuldt <contact@schuldt.dev>
2019-06-30 10:24:11 +0200

    fixed port-forwarding issues

--- k3s/prepare.sh
+++ k3s/prepare.sh
@@ -3,6 +3,7 @@
 # alpine setup
 sudo echo 'default_kernel_opts="quiet rootfstype=ext4 cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 swapaccount=1"' >> /etc/update-extlinux.conf 
 sudo update-extlinux
+sudo echo "127.0.0.1    localhost localhost.localdomain" >> /etc/hosts
 
 # k3s setup
 sudo mv /tmp/k3s / 
