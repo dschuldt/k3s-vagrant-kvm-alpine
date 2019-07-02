0c77fb996a9bcec73e8513960b10c59c4a9f98f4
dschuldt <contact@schuldt.dev>
2019-07-02 20:57:50 +0200

    start k3s server without klipper-lb

--- k3s/start_k3s_server.sh
+++ k3s/start_k3s_server.sh
@@ -3,4 +3,4 @@
 secret='123secret'
 bind_address="$1"
 
-sudo k3s server --bind-address $bind_address --disable-agent --cluster-secret $secret --no-deploy traefik
+sudo k3s server --bind-address $bind_address --disable-agent --cluster-secret $secret --no-deploy traefik --no-deploy servicelb
